712241d9f0b85a7b0fb2a6548d64fefef9e48a18
Nilsson Sebastian <sebastian.nilsson@melitta.se>
2019-10-28 13:31:53 +0100

    changed classmethods

---
+++ F1 Better/app.py
@@ -0,0 +1,7 @@
+import bet
+
+
+better1 = bet.Bet("Sebastian", 200, "HAM", "VET", "BOT")
+better2 = bet.Bet("Filip", 200, "VET", "HAM", "BOT")
+
+
--- F1 Better/bet.py
+++ F1 Better/bet.py
@@ -1,8 +1,14 @@
+# Add bet
+# Follow Race
+# Get current leader during race
+# Get sum of Bet
+# Create a bot
+
 import requests
 import json
 class Bet:
-    totalPot = 0
-    
+
+    pot = 0
 
 
     def __init__(self, name, bet, posOne, posTwo, posThree):
@@ -11,12 +17,33 @@ class Bet:
         self.posOne = posOne
         self.posTwo = posTwo
         self.posThree = posThree
+
+    def checkBets(self):
+        pass
+
+
+   
+    
     
 
 
+    @classmethod
+    def get_race_data(self, endpoint):
+        """Gets JSON data from API"""
+        BASE_URL = 'http://ergast.com/api/f1' 
+        url = "%s/%s"%(BASE_URL, endpoint)
+        r = requests.get(url)
+        final = []
+        if r.status_code == 200:           
+            json_data = json.loads(r.text)
+            return json_data        
+        else:
+            print("Error " + str(r.status_code))
+            return False
 
     @classmethod
     def get_race_results(self):
+        """Gives back a list with the complete table"""
         BASE_URL = 'http://ergast.com/api/f1' 
         url = "%s/current/last/results.json"%(BASE_URL)
         r = requests.get(url)
@@ -37,11 +64,10 @@ class Bet:
         top = Bet.get_race_results()[0] + "\n", Bet.get_race_results()[1] + "\n", Bet.get_race_results()[2]
         return top 
 
+    @classmethod
+    def getResults(self):
+        pass
 
 
 
-
-better1 = Bet("Sebastian", 200, 1,2,2)
-better2 = Bet("Seba", 200, 1,2,2)
-
-print(Bet.getTopThree())
+Bet.get_race_data("current/last/results.json")
